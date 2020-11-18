feeb01819d74fb11e25d4a74ac469ebe5b79954a
CHENntust <46651342+CHENntust@users.noreply.github.com>
2020-11-18 17:38:23 +0800

    Update Adafruit_DHT_demo.py

--- Adafruit_DHT_demo.py
+++ Adafruit_DHT_demo.py
@@ -1,5 +1,16 @@
-import Adafruit_DHT,time
+import dht11,time
+import RPi.GPIO as GPIO
+
+def data_filter(result):
+    while result.humidity==0 and result.temperature==0:
+        #obtain noise data, read snesor data again.
+        result = instance.read()
+    return result.humidity,result.temperature
+
+GPIO.setmode(GPIO.BCM)
+instance = dht11.DHT11(pin=4)
 while True:
-  h, t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4)
-  print('濕度為',h,',溫度為',t,'℃')
-  time.sleep(1)
+    result = instance.read()
+    h, t = data_filter(instance.read())
+    print('濕度為',h,',溫度為',t,'℃')
+    time.sleep(1)
